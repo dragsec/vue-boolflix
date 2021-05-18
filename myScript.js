39efa01a2ffac282bcbdd3bdd8cbb8551462da1a
dragsec <draganyes@gmail.com>
2021-05-18 13:57:37 +0200

    m1

--- myScript.js
+++ myScript.js
@@ -1,14 +1,39 @@
 new Vue({
     el: "#app",
     data: {
-        tmdbApiKey: "0ab1e8bf1bb14d51b6ba7c8ff6e53732"
-        queryToSearch: ""
+        tmdbApiKey: "0ab1e8bf1bb14d51b6ba7c8ff6e53732",
+        textToSearch: ""
+        moviesList: [];
+        tvSeriesList: [];
     },
     methods: {
-        doSearch(){
+        makeAxiosSearch(searchType){
+            const axiosOptions = {
+                params: {
+                    api_key: this.tmdbApiKey,
+                    query: this.textToSearch,
+                    language: "it-IT"
+                }
+            };
 
-            axios.get("https://api.themoviedb.org/3/search/movie?api_key")
+            axios.get("https://api.themoviedb.org/3/search/movie" + searchType, axiosOptions) 
+            .then((resp) => {
+                if (searchType === "movie"){
+                    this.movieList = resp.data.results
+                }else if (searchType === "tv"){
+                    this.tvSeriesList = resp.data.results.map((tvShow) => {
+                        tvShow.original_title = tvShow.original_name;
+                        tvShow.title = tvShow.name;
+                        
+                        return tvShow
+                    });
+                }
+            });
         }
 
+        doSearch(){
+            this.makeAxiosSearch("movie")
+            this.makeAxiosSearch("tv")
+        }
     }
 })
